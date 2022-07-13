08ae991718b248d557a76d57824856e6c651afb6
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-07-12 23:22:58 -0800

    add an extra space thats unnecessary

--- boot-a10.sh
+++ boot-a10.sh
@@ -7,6 +7,7 @@ irecovery -f boot/iBSS.img4
 irecovery -f boot/iBEC.img4
 # execute irecovery -c go to load iBEC image on A10+
 irecovery -c 'go'
+
 irecovery -f boot/devicetree.img4
 irecovery -c devicetree
 irecovery -f boot/trustcache.img4
