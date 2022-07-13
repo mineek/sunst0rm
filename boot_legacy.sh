b3662c4b7a954da213eb4d6a9fbc99ac70ed0288
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-07-12 23:07:59 -0800

    Delete boot_legacy.sh

--- boot_legacy.sh
+++
@@ -1,7 +0,0 @@
-irecovery -f boot/iBSS.img4
-irecovery -f boot/iBSS.img4
-irecovery -f boot/iBEC.img4
-irecovery -f boot/devicetree.img4
-irecovery -c devicetree
-irecovery -f boot/krnlboot.img4
-irecovery -c bootx
