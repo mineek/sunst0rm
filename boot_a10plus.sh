1c52a32669bc384daf1549a067830b67e209564b
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-07-12 22:49:49 -0800

    remove boot_a10plus.sh because it is not needed

--- boot_a10plus.sh
+++
@@ -1,9 +0,0 @@
-irecovery -f boot/iBSS.img4
-irecovery -f boot/iBEC.img4
-irecovery -c go
-irecovery -f boot/devicetree.img4
-irecovery -c devicetree
-irecovery -f boot/trustcache.img4
-irecovery -c firmware
-irecovery -f boot/krnlboot.img4
-irecovery -c bootx
