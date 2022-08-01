d410211924da0306e8124a59e42dda052782433a
Growtopia Jaw <growtopiajaw@gmail.com>
2022-08-02 03:42:51 +0800

    Check if asr64_patcher_ios15 exists

--- sunstorm.py
+++ sunstorm.py
@@ -40,6 +40,9 @@ def dependencies():
     if not os.path.exists('/usr/local/bin/ibootim'):
         print('[!] ibootim not found, please install it')
         sys.exit(1)
+    if not os.path.exists('/usr/local/bin/asr64_patcher_ios15'):
+        print('[!] asr64_patcher_ios15 not found, please install it')
+        sys.exit(1)
 
 def prep_restore(ipsw, blob, board, kpp, legacy, skip_baseband):
     # getting lowercase board to avoid errors
