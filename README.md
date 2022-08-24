e1b79796d0effb52314ed0bccd65bb9066e70c39
Mineek <mineek@mineek.nl>
2022-08-24 09:35:19 +0200

    bash rewrite

--- README.md
+++ README.md
@@ -1,62 +1,11 @@
-# sunst0rm
-iOS Tether Downgrader for checkm8 devices
+# sunst0rm dev
 
-## WARNING: THIS IS THE DEV BRANCH! THIS MAY NOT WORK AND YOU ARE ON YOUR OWN IF YOU USE THE DEV BRANCH!
+## WARNING: THIS IS THE DEV BRANCH, ANYTHING CAN AND WILL GO WRONG!
 
-Based on [my guide](https://github.com/mineek/iostethereddowngrade)
+## NOTE: ANY ISSUES SUBMITTED USING THE DEV BRANCH WILL BE IGNORED!
 
-## Join the [Discord](https://discord.gg/TqVH6NBwS3) server for help
+This is a rewrite of sunst0rm in bash.
 
-## Notes
-- It is a *tethered* downgrade meaning you will have to boot tethered every single time from your PC if your battery dies or if you reboot your phone.
-- On A10-A11 devices crucial functionality such as the Home Button, Audio, Microphone, Vibration does NOT work at the moment.
-- You should NOT be tether downgrading your main device it is only recommended to tether downgrade a second device.
-## Requirements:
-- [libirecovery](https://github.com/libimobiledevice/libirecovery)
-- [futurerestore (fork)](https://github.com/futurerestore/futurerestore)
-- futurerestore must be the nightly build. A compiled binary can be found [here](https://github.com/futurerestore/futurerestore/actions)
-- [iBoot64patcher (fork)](https://github.com/Cryptiiiic/iBoot64Patcher)
-- Precompiled binaries for iBoot64Patcher can be found [here](https://github.com/Cryptiiiic/iBoot64Patcher/actions)
-- [Kernel64patcher (fork)](https://github.com/iSuns9/Kernel64Patcher)
-- [img4tool](https://github.com/tihmstar/img4tool)
-- [img4](https://github.com/xerub/img4lib)
-- [ldid](https://github.com/ProcursusTeam/ldid)
-- [restored_external64_patcher](https://github.com/iSuns9/restored_external64patcher)
-- [asr64_patcher](https://github.com/exploit3dguy/asr64_patcher)
-- [Python3](https://www.python.org/downloads)
-   - Make sure you updated Python and are not using the bundled one in macOS
-- Python dependencies
-   - `pip3 install -r requirements.txt`
-   - A device that is vulnerable to checkm8 (A7-A11 devices.), if your device is not vulnerable then you can *NOT* tether downgrade at all. 
+This is only tested on iPhone 6s (n71map)
 
-**Make sure to use the forks listed above.**
-
-## How to use?
-| Option (short)  | Option (long)               | Description                              |
-|-----------------|-----------------------------|------------------------------------------|
-| `-i IPSW`       | `--ipsw IPSW`               | Path to IPSW                             |
-| `-t SHSH2`      | `--blob SHSH2`              | Path to SHSH2                            |
-| `-r`       | `--restore`            | Restore mode                             |
-| `-b`       | `--boot`               | Boot mode                                |
-| `-d BOARDCONFIG`| `--boardconfig BOARDCONFIG` | BoardConfig to use  (E.g: `d221ap`)      |
-| `-kpp`     | `--kpp`                | Use KPP (A9 or lower)                    |
-| `-id IDENTIFIER`| `--identifier IDENTIFIER`   | Identifier to use  (E.g: `iPhone10,6`)   |
-|                 | `--legacy`             | Use Legacy Mode (iOS 11 or lower)        |
-|                 | `--skip-baseband`           | Skip Baseband sending, do NOT do this if your device does have baseband this argument is only ment to be passed when your device does *not* have baseband such as WiFi only iPads.                  |
-### Restoring
-```py
-python3 sunstorm.py -i 'IPSW' -t 'SHSH2' -r true -d 'BOARDCONFIG'
-```
-- Use `--kpp true` if you have KPP, otherwise don't add
-- A10+ Devices do NOT have KPP so do not add `--kpp true` if you are attempting to tether downgrade an A10+ device, A7-A9X devices does have KPP so that means you will pass `--kpp true` and to clear things up having KPP or not does not change the fact if you are able to tether downgrade your device.
-### Booting
-```py
-python3 sunstorm.py -i 'IPSW' -t 'SHSH2' -b true -d 'BOARDCONFIG' -id 'IDENTIFIER'
-```
-- Use `--kpp true` if you have KPP, otherwise don't add
-```
-./boot.sh
-```
-
-## Credits:
-[M1n1Exploit](https://github.com/Mini-Exploit) - Some code from ra1nstorm
+Usage: ./sunst0rm.sh IPSW BOARDCONFIG
---
+++ sunstorm.sh
@@ -0,0 +1,142 @@
+gaster pwn
+ecid=$(irecovery -q | grep ECID | sed 's/ECID: //')
+if [[ $1 == "boot" ]]; then
+    if [[ -e boot/ibss.img4 ]]; then
+        echo "Found required files, continuing..."
+        irecovery -f boot/ibss.img4
+        irecovery -f boot/ibss.img4
+        irecovery -f boot/ibec.img4
+        irecovery -f boot/devicetree.img4
+        irecovery -c devicetree
+        irecovery -f aop.img4
+        irecovery -c firmware
+        irecovery -f boot/trustcache.img4
+        irecovery -c firmware
+        irecovery -f boot/krnl.img4
+        irecovery -c bootx
+        echo "Device should be booting now."
+        exit
+    else
+        echo "You need to run this script normally first."
+        exit
+    fi
+fi
+if [[ -e work ]]; then
+ rm -rf work
+fi
+mkdir work
+mkdir boot
+if [[ "$1" == "" ]]; then
+ echo "You forgot an ipsw :P"
+ exit
+fi
+if [[ "$2" == "" ]]; then
+ echo "You forgot an boardconfig :P"
+ exit
+fi
+ipsw=$1
+boardconfig=$2
+unzip -q $ipsw -d work
+buildmanifest=$(cat work/BuildManifest.plist)
+firmware=$(/usr/libexec/PlistBuddy -c "Print :ProductVersion" /dev/stdin <<< "$buildmanifest")
+device=$(/usr/libexec/PlistBuddy -c "Print :SupportedProductTypes" /dev/stdin <<< "$buildmanifest")
+device=$(echo $device | grep -oEi "iPod[0-9],1|iPhone[0-9],1|iPad[0-9],1")
+echo "Firmware version: $firmware"
+echo "Device: $device"
+tsschecker -d $device -e $ecid --boardconfig $boardconfig -s -l
+shsh=$(ls *.shsh2)
+echo "Found shsh: $shsh"
+boardconfig_without_ap=$(echo $boardconfig | sed 's/ap//g')
+ibss=$(awk "/"${boardconfig_without_ap}"/{x=1}x&&/iBSS[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')
+ibec=$(awk "/"${boardconfig_without_ap}"/{x=1}x&&/iBEC[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')
+echo "Found iBEC: $ibec"
+echo "Found iBSS: $ibss"
+if [[ -e boot/ibss.img4 ]]; then
+ echo "Skipped making boot files."
+else
+ gaster decrypt work/Firmware/dfu/$ibec work/decrypted_ibec
+ gaster decrypt work/Firmware/dfu/$ibss work/decrypted_ibss
+ iBoot64Patcher work/decrypted_ibss work/ibss.patched
+ iBoot64Patcher work/decrypted_ibec work/ibec.patched -b "-v"
+ img4tool -e -s $shsh -m IM4M
+ img4 -i work/ibss.patched -o boot/ibss.img4 -M IM4M -A -T ibss
+ img4 -i work/ibec.patched -o boot/ibec.img4 -M IM4M -A -T ibec
+ devicetree=$(awk "/"${boardconfig_without_ap}"/{x=1}x&&/DeviceTree[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')
+ img4 -i work/$devicetree -o boot/devicetree.img4 -M IM4M -T rdtr
+ trustcache="$(/usr/libexec/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:StaticTrustCache:Info:Path" | sed 's/"//g')"
+ img4 -i work/$trustcache -o boot/trustcache.img4 -M IM4M -T rtsc 
+ if [[ $device == "iPhone8,"* || $device == "iPhone7,"* || $device == "iPhone6,"* ]]; then
+  echo "Device has kpp"
+  kpp=1
+ else
+  echo "Device does not have kpp"
+  kpp=0
+ fi
+ kernelcache=$(/usr/libexec/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:KernelCache:Info:Path" | sed 's/"//g')
+ if [[ $kpp == 1 ]]; then
+  pyimg4 im4p extract -i work/$kernelcache -o work/kcache.raw --extra work/kpp.bin 
+ fi
+ if [[ $kpp == 0 ]]; then
+  pyimg4 im4p extract -i work/$kernelcache -o work/kcache.raw
+ fi
+ Kernel64Patcher work/kcache.raw work/krnl.patched -f
+ if [[ $kpp == 1 ]]; then
+  pyimg4 im4p create -i work/krnl.patched -o boot/krnl.im4p --extra work/kpp.bin -f rkrn --lzss
+ fi
+ if [[ $kpp == 0 ]]; then
+  pyimg4 im4p create -i work/krnl.patched -o boot/krnl.im4p -f rkrn --lzss
+ fi
+  pyimg4 img4 create -p boot/krnl.im4p -o boot/krnl.img4 -m IM4M
+fi
+if [[ -e restore/krnl.im4p ]]; then
+ echo "Skipped making restore files."
+else
+ ramdisk=$(/usr/libexec/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')
+ echo "Found ramdisk: $ramdisk"
+ img4 -i work/$ramdisk -o work/ramdisk.dmg
+ mkdir work/ramdisk
+ hdiutil attach work/ramdisk.dmg -mountpoint work/ramdisk
+ asr64_patcher work/ramdisk/usr/sbin/asr work/patched_asr
+ ldid -e work/ramdisk/usr/sbin/asr > work/asr.plist
+ ldid -Swork/asr.plist work/patched_asr
+ cp work/ramdisk/usr/local/bin/restored_external work/restored_external
+ restored_external64_patcher work/restored_external work/patched_restored_external
+ ldid -e work/restored_external > work/restored_external.plist
+ ldid -Swork/restored_external.plist work/patched_restored_external
+ chmod -R 755 work/patched_restored_external
+ chmod -R 755 work/patched_asr
+ rm work/ramdisk/usr/sbin/asr
+ rm work/ramdisk/usr/local/bin/restored_external
+ cp work/patched_asr work/ramdisk/usr/sbin/asr
+ cp work/patched_restored_external work/ramdisk/usr/local/bin/restored_external
+ hdiutil detach work/ramdisk
+ mkdir restore
+ pyimg4 im4p create -i work/ramdisk.dmg -o restore/ramdisk.im4p -f rdsk
+ if [[ $device == "iPhone8,"* || $device == "iPhone7,"* || $device == "iPhone6,"* ]]; then
+  echo "Device has kpp"
+  kpp=1
+ else
+  echo "Device does not have kpp"
+  kpp=0
+ fi
+ # get kernelcache from buildmanifest
+ kernelcache=$(/usr/libexec/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:KernelCache:Info:Path" | sed 's/"//g')
+ if [[ $kpp == 1 ]]; then
+  pyimg4 im4p extract -i work/$kernelcache -o work/kcache.raw --extra work/kpp.bin 
+ fi
+ if [[ $kpp == 0 ]]; then
+  pyimg4 im4p extract -i work/$kernelcache -o work/kcache.raw
+ fi
+ Kernel64Patcher work/kcache.raw work/krnl.patched -f -a
+ if [[ $kpp == 1 ]]; then
+  pyimg4 im4p create -i work/krnl.patched -o restore/krnl.im4p --extra work/kpp.bin -f rkrn --lzss
+ fi
+ if [[ $kpp == 0 ]]; then
+  pyimg4 im4p create -i work/krnl.patched -o restore/krnl.im4p -f rkrn --lzss
+ fi
+ echo "Continuing to futurerestore..."
+fi
+echo "Please use this command to restore your device: ( may need to reboot into pwndfu )"
+echo "futurerestore -t $shsh --use-pwndfu --skip-blob --rdsk restore/ramdisk.im4p --rkrn restore/krnl.im4p --latest-sep --latest-baseband $ipsw"
+echo "Then, run the following command to boot your device:"
+echo "./sunstorm.sh boot"
